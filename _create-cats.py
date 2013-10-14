89e96bcdb0b6f9ca991464ab07dd0e456ecb6032
Olaf Gladis <olafgladis@gmail.com>
2013-10-14 18:34:13 +0200

    improved generation speed

--- _create-cats.py
+++ _create-cats.py
@@ -39,12 +39,8 @@ title: Your New Jekyll Site
   <h1>%(catname)s Blog Posts</h1>
 
   <ul class="posts">
-  {%% for category in site.categories %%}
-    {%% if category[0] == "%(catname_raw)s" %%}
-      {%% for post in category[1] %%}
-  <li><span>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ post.url }}">{{ post.title }}</a></li>
-      {%% endfor %%}
-    {%% endif %%}
+  {%% for post in site.categories["%(catname_raw)s"] %%}
+    <li><span>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ post.url }}">{{ post.title }}</a></li>
   {%% endfor %%}
   </ul>
 </div>"""
